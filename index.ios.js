2d5609870641f3db42ab4fd1ed26328210109e5d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-12-07 20:43:02 -0500

    Add progress

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,30 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View
 } from 'react-native';
+import Main from './App/Components/Main'
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
+});
 
 export default class sandbox extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('sandbox', () => sandbox);
